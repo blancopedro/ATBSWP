4b08a015f399da7224be1bf29f0c4403ce61e72b
Pedro Blanco <pedblasan@gmail.com>
2020-04-08 13:11:34 +0200

    ATBSWP

--- Ch 03 - GuessTheNumberImprovements.py
+++ Ch 03 - GuessTheNumberImprovements.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 # Classical game with two improvement. Guess the number in Python + Define the range + Define the number of guesses
 import random
 
@@ -23,3 +24,30 @@ else:
     print("No, the number I was thinking of was " + str(secretNumber))
 
 
+=======
+# Classical game with two improvement. Guess the number in Python + Define the range + Define the number of guesses
+import random
+
+endNumber = input("Please, introduce a end number in order to know until what number you want to play")
+secretNumber = random.randint(1, int(endNumber))
+
+# Ask the player
+numberGuesses = input("Please, introduce how many games do you want to play")
+for guessesTaken in range(1, int(numberGuesses)):
+    print("Take a guess.")
+    guess = int(input())
+
+    if guess < secretNumber:
+        print("Your guess is too low")
+    elif guess > secretNumber:
+        print("Your guess is to high")
+    else:
+         break
+
+if guess == secretNumber:
+    print("Great! Congratulations" + "Your number is correct " + str(guess) + " is the number I wrote down")
+else:
+    print("No, the number I was thinking of was " + str(secretNumber))
+
+
+>>>>>>> 6c2bcee... ATBSWP
---
+++ Week 1.py
@@ -0,0 +1,31 @@
+def hello_func(greeting, name="You"):
+    return '{}, {}'.format(greeting, name)
+print(hello_func("Hi", name="Peter"))
+
+
+# Number of days per month. First value placeholder for indexing purposes.
+month_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+
+def is_leap(year):
+    """Return True for leap years, False for non-leap years."""
+
+    return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
+
+
+def days_in_month(year, month):
+    """Return number of days in that month in that year."""
+
+    # year 2017
+    # month 2
+    if not 1 <= month <= 12:
+        return 'Invalid Month'
+
+    if month == 2 and is_leap(year):
+        return 29
+
+    return month_days[month]
+
+print("El mes del año es " + days_in_month(2020, 2), is_leap(2021))
+
+
