f5f4e48c0a06fa74c8c0aaa47787e232d790bffa
Pedro Blanco <pedblasan@gmail.com>
2020-04-08 19:52:05 +0200

    Initial commit

--- Week 1.py
+++ Week 1.py
@@ -13,7 +13,7 @@ def is_leap(year):
     return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
 
 
-def days_in_month(year, month):
+def days_in_month(year, month): 
     """Return number of days in that month in that year."""
 
     # year 2017
